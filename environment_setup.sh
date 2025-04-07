d8b0efbca9f3e0c0166ebc1e5942d27c84bd0a37
Fr0zenCrane <qinluozheng@sais.com.cn>
2025-04-07 16:25:40 +0800

    Update environment_setup.sh

--- environment_setup.sh
+++ environment_setup.sh
@@ -2,6 +2,7 @@
 conda create -n cockatiel python==3.10
 conda activate cockatiel
 pip install -e .
+pip install -e ".[train]"
 pip install transformers==4.37.2
 pip install torch==2.1.2 torchvision==0.16.2
 pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.5.8/flash_attn-2.5.8+cu122torch2.1cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
