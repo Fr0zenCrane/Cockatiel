e40bb488444180f142c4bd562386526c7ec1a20f
Fr0zenCrane <qinluozheng@sais.com.cn>
2025-04-07 16:28:25 +0800

    Update environment_setup.sh

--- environment_setup.sh
+++ environment_setup.sh
@@ -6,6 +6,7 @@ pip install -e ".[train]"
 pip install transformers==4.37.2
 pip install torch==2.1.2 torchvision==0.16.2
 pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.5.8/flash_attn-2.5.8+cu122torch2.1cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
+pip install git+https://github.com/bfshi/scaling_on_scales.git
 site_pkg_path=$(python -c 'import site; print(site.getsitepackages()[0])')
 cp -rv ./llava/train/transformers_replace/* $site_pkg_path/transformers/
 cp -rv ./llava/train/deepspeed_replace/* $site_pkg_path/deepspeed/
